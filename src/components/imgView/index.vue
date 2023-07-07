5186752f4a23bb6bb6931ca9853b91839ed5ce89
张大大 <2458015575@qq.com>
2023-07-07 18:15:51 +0800

    详情页-大图预览完成

--- src/components/imgView/index.vue
+++ src/components/imgView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from "vue"
+import { ref, watch } from "vue";
+import { useMouseInElement } from '@vueuse/core';
 
 // 图片列表
 const imageList = [
@@ -11,10 +12,22 @@ const imageList = [
 ]
 
 const activeIndex = ref(0);
-
 const enterHandle = (i) => {
   activeIndex.value = i;
 }
+const left = ref(0);
+const top = ref(0);
+const target = ref(null);
+const {elementX, elementY, isOutside} = useMouseInElement(target);
+watch([elementX, elementY, isOutside], () => {
+  if (elementX.value > 100 && elementX.value < 300) left.value = elementX.value - 100;
+  if (elementY.value > 100 && elementY.value < 300) top.value = elementY.value - 100;
+  if (elementX.value < 100) left.value = 0;
+  if (elementX.value > 300) left.value = 200;
+  if (elementY.value < 100) top.value = 0;
+  if (elementY.value > 300) top.value = 200;
+});
+
 </script>
 
 
@@ -24,7 +37,7 @@ const enterHandle = (i) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div v-show="!isOutside" class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -35,11 +48,11 @@ const enterHandle = (i) => {
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
+        backgroundPositionX: `-${left*2}px`,
+        backgroundPositionY: `-${top*2}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
