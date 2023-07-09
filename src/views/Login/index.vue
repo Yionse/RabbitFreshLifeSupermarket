6c6d5a9ab8cccba3529a603fe25c7003439b1f3f
张大大 <2458015575@qq.com>
2023-07-09 14:20:04 +0800

    登录页-表单校验，勾选按钮

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,7 +3,8 @@ import {ref} from 'vue';
 
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree: true
 });
 
 const rules = {
@@ -13,6 +14,17 @@ const rules = {
   password: [
     { required: true, message: '密码不能为空', trigger: 'blur'},
     { min: 4, max: 16, message: '密码长度4-16位', trigger: 'change'}
+  ],
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        if (value) {
+          callback();
+        }else {
+          callback(new Error('请勾选同意协议！'));
+        }
+      }
+    }
   ]
 };
 </script>
@@ -47,8 +59,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  v-model="form.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
